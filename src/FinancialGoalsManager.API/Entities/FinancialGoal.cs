5d2c4ba97c42d6e98fd228639e5e3c425471a146
Renan Martins <renanmartins1999@hotmail.com>
2025-08-10 16:05:27 -0300

    feat: add constructor and update method to entity

--- src/FinancialGoalsManager.API/Entities/FinancialGoal.cs
+++ src/FinancialGoalsManager.API/Entities/FinancialGoal.cs
@@ -1,12 +1,24 @@
 namespace FinancialGoalsManager.API.Entities
 {
-    public class FinancialGoal
+    public record FinancialGoal
     {
+        public FinancialGoal(string? title, decimal targetQuantity)
+        {
+            Title = title;
+            TargetQuantity = targetQuantity;
+        }
+
         public int Id { get; set; }
         public string? Title { get; set; }
         public decimal TargetQuantity { get; set; }
         public ICollection<Transaction>? Transactions { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsDeleted { get; set; }
+
+        public void Update(string? title, decimal targetQuantity)
+        {
+            Title = title;
+            TargetQuantity = targetQuantity;
+        }
     }
 }
