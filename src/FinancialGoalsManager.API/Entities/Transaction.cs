4df7ac370c6a269a0b05211a42b46fa458093fab
Renan Martins <renanmartins1999@hotmail.com>
2025-08-24 20:48:43 -0300

    feat: add constructor in Transaction.cs

--- src/FinancialGoalsManager.API/Entities/Transaction.cs
+++ src/FinancialGoalsManager.API/Entities/Transaction.cs
@@ -1,10 +1,20 @@
+using System.Text.Json.Serialization;
 using FinancialGoalsManager.API.Enums;
 
 namespace FinancialGoalsManager.API.Entities
 {
     public class Transaction
     {
+        public Transaction(int financialGoalId, decimal quantity, TransactionTypeEnum transactionType)
+        {
+            Quantity = quantity;
+            TransactionType = transactionType;
+            FinancialGoalId = financialGoalId;
+            CreatedAt =  DateTime.UtcNow;
+        }
+
         public int Id { get; set; }
+        public int FinancialGoalId { get; set; }
         public decimal Quantity { get; set; }
         public TransactionTypeEnum TransactionType { get; set; }
         public DateTime CreatedAt { get; set; }
