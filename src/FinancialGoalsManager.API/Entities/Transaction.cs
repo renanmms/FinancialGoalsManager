0c1e3cb67ea44bee5718081a71facdb6e02d63e6
Renan Martins <renanmartins1999@hotmail.com>
2025-08-30 18:02:31 -0300

    feat: add navigation property

--- src/FinancialGoalsManager.API/Entities/Transaction.cs
+++ src/FinancialGoalsManager.API/Entities/Transaction.cs
@@ -1,8 +1,10 @@
 using System.Text.Json.Serialization;
 using FinancialGoalsManager.API.Enums;
+using Microsoft.VisualBasic;
 
 namespace FinancialGoalsManager.API.Entities
 {
+    // TODO: Change class name
     public class Transaction
     {
         public Transaction(int financialGoalId, decimal quantity, TransactionTypeEnum transactionType)
@@ -15,6 +17,7 @@ namespace FinancialGoalsManager.API.Entities
 
         public int Id { get; set; }
         public int FinancialGoalId { get; set; }
+        public FinancialGoal FinancialGoal { get; set; }
         public decimal Quantity { get; set; }
         public TransactionTypeEnum TransactionType { get; set; }
         public DateTime CreatedAt { get; set; }
